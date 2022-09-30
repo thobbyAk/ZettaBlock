f03a7bbd9f67f597e44c7e739049faafe6d47859
Oluwatobi Akinrinade <tobi@dystopialabs.com>
2022-09-30 20:47:05 +0100

    field clean up

--- schema/schema.js
+++ schema/schema.js
@@ -1,120 +1,51 @@
 const graphql = require("graphql");
-const _ = require("lodash");
+const getUrlTitle = require("get-url-title");
 
-const {
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLSchema,
-	GraphQLID,
-	GraphQLInt,
-	GraphQLList,
-} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLList } =
+	graphql;
 
-// dummy data
-var books = [
-	{ name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
-	{ name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
-	{ name: "The Final Empire", genre: "Fantasy", id: "3", authorId: "2" },
-	{ name: "The new Earth", genre: "Sci-Fi", id: "4", authorId: "3" },
-	{ name: "The Long Moon", genre: "Sci-Fi", id: "5", authorId: "3" },
-	{ name: "The short Pluto", genre: "Sci-Fi", id: "6", authorId: "3" },
-];
+const GetUrlTitle = async (urltofetch) => {
+	const response = await getUrlTitle(urltofetch);
+	return response;
+};
 
-var authors = [
-	{ name: "Brendan Shaw", age: 102, id: "1" },
-	{ name: "Mohamed Li", age: 32, id: "2" },
-	{ name: "Yu Yang", age: 21, id: "3" },
-];
-
-const BookType = new GraphQLObjectType({
-	name: "Book",
+const LinkType = new GraphQLObjectType({
+	name: "Links",
 	fields: () => ({
-		id: { type: GraphQLID },
-		name: { type: GraphQLString },
-		genre: { type: GraphQLString },
-		author: {
-			type: AuthorType,
-			resolve(parent, args) {
-				return _.find(authors, { id: parent.authorId });
-			},
-		},
+		url: { type: GraphQLString },
+		title: { type: GraphQLString },
 	}),
 });
 
-// const EmoticonsType = new GraphQLObjectType({
-//     name : "Emoticons",
-//     fields: () => ({
-
-//     })
-// })
-// const LinksType = new GraphQLObjectType({
-//     name : "Links",
-//     fields: () => ({
-
-//     })
-// })''
-
-const AuthorType = new GraphQLObjectType({
-	name: "Author",
-	fields: () => ({
-		id: { type: GraphQLID },
-		name: { type: GraphQLString },
-		age: { type: GraphQLInt },
-		books: {
-			type: new GraphQLList(BookType),
-			resolve(parent, args) {
-				return _.filter(books, { authorId: parent.id });
-			},
-		},
-	}),
-});
-
-const MentionType = new GraphQLObjectType({
-	name: "Mentions",
+const RecordType = new GraphQLObjectType({
+	name: "Records",
 	fields: () => ({
 		mentions: { type: new GraphQLList(GraphQLString) },
+		emoticons: { type: new GraphQLList(GraphQLString) },
+		links: {
+			type: new GraphQLList(LinkType),
+		},
 	}),
 });
 
 const RootQuery = new GraphQLObjectType({
 	name: "RootQueryType",
 	fields: {
-		book: {
-			type: BookType,
-			args: { id: { type: GraphQLID } },
-			resolve(parent, args) {
-				//code to get code from db /other source
-				return _.find(books, { id: args.id });
-			},
-		},
-		author: {
-			type: AuthorType,
-			args: { id: { type: GraphQLID } },
-			resolve(parent, args) {
-				return _.find(authors, { id: args.id });
-			},
-		},
-		books: {
-			type: new GraphQLList(BookType),
-			resolve(parent, args) {
-				return books;
-			},
-		},
-		authors: {
-			type: new GraphQLList(AuthorType),
-			resolve(parent, args) {
-				return authors;
-			},
-		},
 		records: {
-			type: new GraphQLList(GraphQLString),
+			type: RecordType,
 			args: { message: { type: GraphQLString } },
 			resolve(parent, args) {
-				console.log(
-					"mentions",
-					args.message.match(/@\w+/g).map((x) => x.substr(1))
-				);
-				return args.message.match(/@\w+/g).map((x) => x.substr(1));
+				return {
+					mentions: args.message.match(/@\w+/g)?.map((x) => x.substr(1)) || [],
+					emoticons:
+						args.message.match(/\(.*?\)/g)?.map((x) => x.replace(/[()]/g, "")) || [],
+					links: [
+						{
+							url: args.message.match(/\bhttps?:\/\/\S+/gi)?.[0],
+							title: GetUrlTitle(args.message.match(/\bhttps?:\/\/\S+/gi)?.[0]),
+						},
+					],
+				};
 			},
 		},
 	},
